9aeabc2477deceefa61907879c98fdd165e52835
Budi Darmawan <budi20.darmawan20@gmail.com>
2021-08-13 08:06:25 +0700

    Initial Commit

---
+++ Game Catalogue SUI/Game_Catalogue_SUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Game_Catalogue_SUIApp.swift
+//  Game Catalogue SUI
+//
+//  Created by Budi Darmawan on 13/08/21.
+//
+
+import SwiftUI
+
+@main
+struct Game_Catalogue_SUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
